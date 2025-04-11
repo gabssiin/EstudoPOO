23f01e8070be918dd490262a089f67790ef33ebc
gabriel santos <163359098+gabssiin@users.noreply.github.com>
2025-04-11 23:04:43 +0000

    sa

---
+++ ClassAbstrata1/TrabalhadorAutonomo.java
@@ -0,0 +1,20 @@
+package ClassAbstrata1;
+
+public class TrabalhadorAutonomo  extends Trabalhador{
+    public TrabalhadorAutonomo(){
+
+    }
+
+    public TrabalhadorAutonomo(String nome, double salarioBase){
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public void calcularSalarioMensal(){
+        if (condition) {
+            
+        } else {
+            
+        }
+    }
+}
